b9e82608877aa9bea284eb43ce98d7b49b417919
Agatha França <agathafranca@outlook.com>
2023-03-27 21:35:48 -0300

    Cria novos casos de teste para a funcionalidade login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -45,5 +45,50 @@ describe('login', () => {
 
             loginPage.noticeShouldBe(message)
         })
+
+        it('campos obrigatórios', () => {
+            loginPage.submit()
+
+            loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
+        })
+    })
+
+    context('senha muito curta', () => {
+        const passwords = [
+            '1',
+            '12',
+            '123',
+            '1234',
+            '12345'
+        ]
+
+        passwords.forEach((p) => {
+            it(`não deve logar com a senha: ${p}`, () => {
+                loginPage.submit('agathafranca@outlook.com', p)
+
+                loginPage.alertShouldBe('Pelo menos 6 caracteres')
+            })
+        })
+    })
+
+    context('email no formato incorreto', () => {
+        const emails = [
+            'agatha&gmail.com',
+            'agatha.com.br',
+            '@gmail.com',
+            '@',
+            'agatha@',
+            '121323',
+            '@#@!3!@',
+            'xpto123'
+        ]
+
+        emails.forEach((e) => {
+            it(`não deve logar com o email no formato incorreto: ${e}`, () => {
+                loginPage.submit(e, 'pwd456')
+
+                loginPage.alertShouldBe('Informe um email válido')
+            })
+        })
     })
 })
