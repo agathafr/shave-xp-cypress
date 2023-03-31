e9d520675e3bafc02b4ba204dc37c8918f82dda6
Agatha França <agathafranca@outlook.com>
2023-03-31 20:03:40 -0300

    Cria rota delete no arq app.js migra conexão ao banco para db.js
    Cria uma rota que ao ser invocada através do método HTTP DELETE,
    remove um usuário usando o e-mail como parâmetro. E concentra a importação
    da classe que passa o objeto que guarda os dados para conectar ao banco,
    o próprio objeto, a função async que deleta por e-mail e a exportação dessa
    função no arquivo db.js, que foi criado na camada de api.

--- api/app.js
+++ api/app.js
@@ -3,8 +3,16 @@ const app = express()
 
 app.use(express.json())
 
+const { deleteUser } = require('./db')
+
 app.get('/', function (req, res) {
-  res.json({message: 'Olá QAx'})
+    res.json({ message: 'Olá QAx' })
+})
+
+app.delete('/user/:email', async function (req, res) {
+    const { email } = req.params
+    await deleteUser(email)
+    res.status(204).end()
 })
 
 app.listen(5000)
---
+++ api/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg')
+
+const dbConfig = {
+    host: 'motty.db.elephantsql.com',
+    user: 'ukubjzju',
+    password: 'fNWQk7e4z760FlJrjOTZSBqVke0kf23T',
+    database: 'ukubjzju',
+    port: 5432
+}
+
+const pool = new Pool(dbConfig)
+
+async function deleteUser(email) {
+    await pool.query('DELETE FROM users WHERE email = $1', [email])
+}
+
+module.exports = {
+    deleteUser
+}
