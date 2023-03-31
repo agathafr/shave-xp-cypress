55f1916c17aca7ad240d40f74af2c376f9caee6a
Agatha França <agathafranca@outlook.com>
2023-03-31 07:38:31 -0300

    Implementação da task que remove um usuário através do email

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,30 @@
 const { defineConfig } = require("cypress");
 
+const { Pool } = require('pg')
+
+const dbConfig = {
+  host: 'motty.db.elephantsql.com',
+  user: 'ukubjzju',
+  password: 'fNWQk7e4z760FlJrjOTZSBqVke0kf23T',
+  database: 'ukubjzju',
+  port: 5432
+}
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        removeUser(email) {
+          const pool = new Pool(dbConfig)
+
+          pool.query('DELETE FROM users WHERE email = $1', [email], function(error, result){
+            if (error) {
+              throw error
+            }
+          })
+        }
+      })
     },
     viewportWidth: 1920,
     viewportHeight: 1080,
