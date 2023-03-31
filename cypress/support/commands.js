fc96518bc756d0faeb0e2352a44c183776140756
Agatha França <agathafranca@outlook.com>
2023-03-31 20:17:10 -0300

    Remove a chamada da task removeUser
    A chamada à task removeUser não será usada mais e portanto, foi removida.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,11 +25,6 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('createUser', (user) => {
-    cy.task('removeUser', user.email)
-        .then(function (result) {
-            cy.log(result)
-        })
-
     cy.request({
         method: 'POST',
         url: 'http://localhost:3333/users',
