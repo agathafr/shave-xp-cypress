fcd57aa78dbeca80472d8f946885ae0f662b70a5
Agatha França <agathafranca@outlook.com>
2023-03-31 15:12:53 -0300

    Define que a api vai trabalhar com a representação JSON
    Ao invés da resposta da api ser um texto (res.send), será um json (res.json).
    Não será enviada uma string res.send('Olá QAx'), será enviado um objeto
    javascript ({message: 'Olá QAx'}). Este será exibido na aplicação no formato
    JSON.

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', function (req, res) {
-  res.send('Olá QAx')
+  res.json({message: 'Olá QAx'})
 })
 
 app.listen(5000)
