18f8050decc280c74e822cf4d0be639296145bef
Agatha França <agathafranca@outlook.com>
2023-03-31 08:35:56 -0300

    Altera localização da função que implementa uma task
    Retira a função que implementa a task de remover o usuário do banco de
    dados do arquivo cypress.config.js e coloca no arquivo database.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,18 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {
-        removeUser(email) {
-          return new Promise(function (resolve) {
-            const pool = new Pool(dbConfig)
-
-            pool.query('DELETE FROM users WHERE email = $1', [email], function (error, result) {
-              if (error) {
-                throw error
-              }
-              resolve({ success: result })
-            })
-          })
-        }
+        
       })
     },
     viewportWidth: 1920,
