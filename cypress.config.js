decfef141a3dbd51a8060a518c01022fbbbfe8cd
Agatha França <agathafranca@outlook.com>
2023-03-31 08:28:51 -0300

    Altera localização de um trecho de código
    Retira o trecho de código que importa o módulo do pg, e que monta o
    objeto que tem a conexão com o banco de dados do arquvo cypress.config.js
    e coloca no arquvo database.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,5 @@
 const { defineConfig } = require("cypress");
 
-const { Pool } = require('pg')
-
-const dbConfig = {
-  host: 'motty.db.elephantsql.com',
-  user: 'ukubjzju',
-  password: 'fNWQk7e4z760FlJrjOTZSBqVke0kf23T',
-  database: 'ukubjzju',
-  port: 5432
-}
-
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
---
+++ cypress/support/tasks/database.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg')
+
+const dbConfig = {
+  host: 'motty.db.elephantsql.com',
+  user: 'ukubjzju',
+  password: 'fNWQk7e4z760FlJrjOTZSBqVke0kf23T',
+  database: 'ukubjzju',
+  port: 5432
+}
