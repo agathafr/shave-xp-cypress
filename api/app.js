a23d24353e42cb8da9365d62fd8c8f52d5ce3afa
Agatha França <agathafranca@outlook.com>
2023-04-01 09:08:51 -0300

    Instala, importa e testa o módulo que criptografa a senha
    Foi feita a instalação do módulo bcrypt como dependência de desenvolvimento
    através do comando: npm install bcrypt --save-dev. O import
    const bcrypt = require('bcrypt'), o acesso ao método que criptografa
    dados const hashPass = await bcrypt.hash(password, 8), bem como o envio
    do dado a ser criptografado, e a gravação numa constante para validar,
    através do console.log(hashPass), se foi feita a criptografia conforme
    o esperado. Antes ainda, foi feito o teste se o request body enviado
    pelo cliente (insomnia) estava acessível e foi feita a atribuição
    dos valores à variáveis para posterior utilização. Feitos representados
    pelas linhas de código a seguir:
    console.log(req.body)
    const { name, email, password, is_shaver } = req.body

--- api/app.js
+++ api/app.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const express = require('express')
 const app = express()
 
@@ -13,6 +14,18 @@ app.delete('/user/:email', async function (req, res) {
     const { email } = req.params
     await deleteUser(email)
     res.status(204).end()
+})
+
+app.post('/user', async function (req, res) {
+    console.log(req.body)
+
+    const { name, email, password, is_shaver } = req.body
+
+    const hashPass = await bcrypt.hash(password, 8)
+
+    console.log(hashPass)
+
+    res.status(200).end()
 })
 
 app.listen(5000)
