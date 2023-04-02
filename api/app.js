c4afb1ad131baf5fa4dcba76cfc77bf9d923648e
Agatha França <agathafranca@outlook.com>
2023-04-02 08:59:08 -0300

    Implementa interceptador para customizar erros
    O interceptador customiza as mensagens de erro que são validadas pela
    biblioteca joi. Todas chamadas à api irão cair na interceptação.
    if -err && err.error && err.error.isJoi- valida se o objeto err existe,
    se existir, valida se ele tem a propriedade error e se tiver, verifica
    se ela é do tipo Joi. Caso todas as condições retornem true, é um indício
    houve falha em validações implementadas no userShema. if retornando true,
    vai mostrar o status 400 e msg do erro que ocorreu. if retornando false,
    a implementação do else é executada dando seguimento na verificação dos
    erros.

--- api/app.js
+++ api/app.js
@@ -49,4 +49,17 @@ app.post('/user', validator.body(userSchema), async function (req, res) {
     }
 })
 
+app.use((err, req, res, next) => {
+    if (err && err.error && err.error.isJoi) {
+        // we had a joi error, let's return a custom 400 json response
+        res.status(400).json({
+            type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+            message: err.error.toString()
+        });
+    } else {
+        // pass on to another error handler
+        next(err);
+    }
+});
+
 app.listen(5000)
