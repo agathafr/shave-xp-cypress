bbb2778df7206c2cd3bdb228d2031b6abc98264d
Agatha França <agathafranca@outlook.com>
2023-04-02 10:20:34 -0300

    Invoca as funções da camada page na camada de teste
    Agora, as funções criadas na camada de page estão sendo chamadas na camada de
    testes

--- cypress/e2e/recovery-pass.cy.js
+++ cypress/e2e/recovery-pass.cy.js
@@ -1,7 +1,11 @@
 import forgotPass from '../support/pages/forgot-pass'
 
-describe('esqueci minha senha', ()=> {
-    it('deve poder solicitar o resgate de senha', ()=> {
-        
+describe('esqueci minha senha', () => {
+    it('deve poder solicitar o resgate de senha', () => {
+        forgotPass.go()
+        forgotPass.submit()
+
+        const message = 'Enviamos um e-mail para confirmar a recuperação de senha, verifique sua caixa de entrada.'
+        forgotPass.noticeShouldBe(message)
     })
 })
