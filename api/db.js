8e816422d61283ffe6dc0c50c343434a0e916636
Agatha <85461130+agathafr@users.noreply.github.com>
2023-04-06 20:59:06 -0300

    Create db.js
    Cria arquivo que guarda as operações feitas no banco sem os dados que usados para conectar ao banco

---
+++ api/db.js
@@ -0,0 +1,46 @@
+const { Pool } = require('pg')
+
+const dbConfig = {
+    host: '',
+    user: '',
+    password: '',
+    database: '',
+    port: ''
+}
+
+const pool = new Pool(dbConfig)
+
+async function deleteUser(email) {
+    await pool.query('DELETE FROM users WHERE email = $1', [email])
+}
+
+async function insertUser(user) {
+    const sql = 'INSERT INTO users (name, email, password, is_shaver) VALUES ($1, $2, $3, $4) returning id'
+    const data = [user.name, user.email, user.password, user.is_shaver]
+
+    const result = await pool.query(sql, data)
+    const { id } = result.rows[0]
+
+    return id
+}
+
+async function findToken(email) {
+    const sql = 'SELECT B.token' +
+        ' FROM users A' +
+        ' INNER JOIN user_tokens B' +
+        ' ON A.id = B.user_id' +
+        ' WHERE A.email = $1' +
+        ' ORDER BY B. created_at DESC LIMIT 1'
+
+    const result = await pool.query(sql, [email])
+
+    console.log(result.rows[0])
+
+    return result.rows[0]
+}
+
+module.exports = {
+    deleteUser,
+    insertUser,
+    findToken
+}
