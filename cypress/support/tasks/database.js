707f26cc0bc5db642d2bf1645ee56699418987b7
Agatha França <agathafranca@outlook.com>
2023-04-08 07:55:05 -0300

    Adiciona task com operação que deleta usuário

---
+++ cypress/support/tasks/database.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg')
+
+require("dotenv/config");
+
+const dbConfig = {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_NAME,
+    port: 5432
+}
+
+module.exports = {
+    removeUser(email) {
+        return new Promise(function (resolve) {
+            const pool = new Pool(dbConfig)
+
+            pool.query('DELETE FROM users WHERE email = $1', [email], function (error, result) {
+                if (error) {
+                    throw error
+                }
+                resolve({ success: result })
+            })
+        })
+    }
+}
