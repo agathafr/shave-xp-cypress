08e6ce9673985dfd8d218a4ca2913e5495f7915c
Agatha França <agathafranca@outlook.com>
2023-04-09 09:09:47 -0300

    Retira timeout da busca do elemento

--- cypress/support/pages/shared/index.js
+++ cypress/support/pages/shared/index.js
@@ -1,16 +1,16 @@
 class SharedSteps {
 
     noticeErrorShouldBe(message) {
-        cy.get('.notice-container', { timeout: 10000 })
+        cy.get('.notice-container')
             .should('be.visible')
-            .find('.error p', { timeout: 10000 })
+            .find('.error p')
             .should('have.text', message)
     }
 
     noticeSuccessShouldBe(message) {
         cy.get('.notice-container', { timeout: 10000 })
             .should('be.visible')
-            .find('.success p', { timeout: 10000 })
+            .find('.success p')
             .should('have.text', message)
     }
 
