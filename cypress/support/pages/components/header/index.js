b72a5f47ce547ca4b08b53bed6ffbc6ba5010e00
Agatha França <agathafranca@outlook.com>
2025-10-18 15:06:00 -0300

    Add timeout to user login visibility check in header component

--- cypress/support/pages/components/header/index.js
+++ cypress/support/pages/components/header/index.js
@@ -1,7 +1,7 @@
 class Header {
     userShouldLoggedIn(name) {
         const firstName = name.split(' ')[0]
-        cy.get('.logged-user div a')
+        cy.get('.logged-user div a', { timeout: 20000 })
             .should('be.visible')
             .should('have.text', 'Olá, ' + firstName)
     }
