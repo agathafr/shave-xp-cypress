1e2e3f7feb53711f696ac762d036b82728f7a22e
Agatha França <agathafranca@outlook.com>
2025-10-18 15:05:30 -0300

    Add timeout to user login visibility check

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -19,7 +19,7 @@ Cypress.Commands.add('submitLogin', (email = null, password = null) => {
 Cypress.Commands.add('userShouldBeLoggedIn', (name) => {
     const firstName = name.split(' ')[0]
 
-    cy.get('.logged-user div a')
+    cy.get('.logged-user div a', { timeout: 20000 })
         .should('be.visible')
         .should('have.text', 'Olá, ' + firstName)
 })
